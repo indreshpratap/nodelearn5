3c6bca914c0ea358cbeb8682579d7f50b1069e95
Indresh <ips.indresh@gmail.com>
2019-04-07 17:39:08 +0530

    third party example

---
+++ src/4-thirdparty.js
@@ -0,0 +1,13 @@
+var colors = require('colors');
+var shortId = require('shortid');
+console.log('I am in green color'.green);
+console.log('This is cyan'.cyan);
+
+
+console.log('Test bg'.bgMagenta);
+
+console.log(shortId.generate());
+console.log(shortId.generate());
+console.log(shortId.generate());
+console.log(shortId.generate());
+console.log(shortId.generate());
