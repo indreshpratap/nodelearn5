cad1dd70c39bf5cafef7737c66396e12f0e0f8c7
Indresh <ips.indresh@gmail.com>
2019-05-05 18:14:34 +0530

    view engine + static resources

--- src/server.js
+++ src/server.js
@@ -1,17 +1,43 @@
 var express = require('express');
+var nunjucks = require('nunjucks');
+var path = require('path');
 //require('./examples/1-module');
 var app = express();
+
+//app.use("/resources",express.static(path.resolve(__dirname,'assets')));
+app.use(express.static(path.resolve(__dirname,'assets')));
+// serving from node_modules specific to bulma
+app.use('/resources/bulma/css',express.static(path.resolve(__dirname,'..','node_modules','bulma','css')));
+nunjucks.configure(path.resolve(__dirname, 'views'),
+    {
+        express: app,  // nunjucks to express integration
+        autoescape: true,
+        noCache: false,
+        watch: true
+    }
+);
 
 
 app.get("/", (request, response) => {
     console.log(request.url);
-    response.send("Test express");
+    response.render("index.html",{title:"Index page"});
 });
 
 
 app.get("/home", (request, response) => {
     console.log(request.url);
-    response.send("Home page");
+    response.render("home.html", 
+    {
+     username: 'Indresh',
+     currentTime: new Date() ,
+     todoItems : [
+         {label:"First todo item",completed:false},
+         {label:"First todo item 2",completed:true},
+         {label:"First todo item 3",completed:false},
+         {label:"First todo item 4",completed:false},
+     ]
+  //   title:"Home page"
+    });
 });
 
 //query parameter ?
@@ -21,7 +47,7 @@ app.get("/search", (request, response) => {
 });
 
 // Params
-app.get("/product/:id/:name/details",(request,response)=>{
+app.get("/product/:id/:name/details", (request, response) => {
     console.log(request.url);
     response.send(request.params);
 })
@@ -30,18 +56,18 @@ app.get("/data", (request, response) => {
     response.json({ name: "xyz", message: "This is message" });
 });
 
-app.get("/process",(request,response)=>{
+app.get("/process", (request, response) => {
     response.status(500).send('Not a valid request');
 });
 
-app.get("/process-json",(request,response)=>{
-    response.status(500).json({error:'Not a valid request'});
+app.get("/process-json", (request, response) => {
+    response.status(500).json({ error: 'Not a valid request' });
 });
 
 
 // wildcard handler always keep at bottom
-app.get("/**",(request,response)=>{
-    response.status(404).send("Requested url not found "+ request.url);
+app.get("/**", (request, response) => {
+    response.status(404).send("Requested url not found " + request.url);
 });
 
 app.listen(3000, () => {
